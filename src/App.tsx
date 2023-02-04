2d7759b27dbe7720e646af5880d8eb3c76ce4ce5
erdemir123 <sadikerdemir93@gmail.com>
2023-02-05 01:06:48 +0300

    app designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
 import axios from "axios";
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react";
 import { CountryType } from "./types";
 
-
 function App() {
-  const [countries,setCountries]=useState<CountryType[]>([])
-  const [load,setLoad]=useState<boolean>(false)
-  const getCountries=async()=>{
+  const [countries, setCountries] = useState<CountryType[]>([]);
+  const [load, setLoad] = useState<boolean>(false);
+  const getCountries = async () => {
     try {
-      setLoad(true)
-      const {data} = await axios.get<CountryType[]>("https://restcountries.com/v2/all")
-      setCountries(data)
-      console.log(countries)
-      console.log("load", load)
+      setLoad(true);
+      const { data } = await axios.get<CountryType[]>(
+        "https://restcountries.com/v2/all"
+      );
+      setCountries(data);
     } catch (error) {
       console.log("error");
+    } finally {
+      setLoad(false);
     }
-    finally{
-      setLoad(false)
-    }
-    
-  }
+  };
   useEffect(() => {
-    getCountries()
-  }, [])
-  
+    getCountries();
+  }, []);
+  console.log("load", load);
+  console.log(countries);
   return (
     <div className="App">
-      Countries
+      {load && "sadık"}
+      {countries.map((country) => {
+        return(
+          <p>
+            {country.name} / {country.capital}
+          </p>
+        )
+      })}
     </div>
   );
 }
