37d0d901441c87f13d2e0866fd3c75b17b4b2e24
erdemir123 <sadikerdemir93@gmail.com>
2023-02-05 00:55:37 +0300

    country type added and project skelteon creates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useState,useEffect} from "react"
+import { CountryType } from "./types";
+
 
 function App() {
+  const [countries,setCountries]=useState<CountryType[]>([])
+  const [load,setLoad]=useState<boolean>(false)
+  const getCountries=async()=>{
+    try {
+      setLoad(true)
+      const {data} = await axios.get<CountryType[]>("https://restcountries.com/v2/all")
+      setCountries(data)
+      console.log(countries)
+      console.log("load", load)
+    } catch (error) {
+      console.log("error");
+    }
+    finally{
+      setLoad(false)
+    }
+    
+  }
+  useEffect(() => {
+    getCountries()
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Countries
     </div>
   );
 }
